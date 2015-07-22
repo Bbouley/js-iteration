3797bf0755578060cad5328dbe35c60f4657f3d3
bbouley <b.bouley@hotmail.co.uk>
2015-07-21 19:51:08 -0600

    finished name page with full onclick functionality

--- js/dom2.js
+++ js/dom2.js
@@ -2,4 +2,46 @@ var names = document.getElementsByClassName('all-names');
 
 var arrayNames = convertNamesToArray(names[0].innerHTML);
 
-console.log(arrayNames);
+var answerBox = document.getElementById('answer');
+
+var printNames = document.getElementById('all-names');
+
+var firstNames = document.getElementById('first-names');
+
+var lastNames = document.getElementById('last-names');
+
+var namesWithLength = document.getElementById('names-with-lengths');
+
+printNames.onclick = function(){
+  for (var i = 0; i < arrayNames.length; i++) {
+    var p = document.createElement('p');
+    p.innerHTML = arrayNames[i];
+    answerBox.appendChild(p);
+  }
+};
+
+firstNames.onclick = function(){
+  for (var i = 0; i < arrayNames.length; i++) {
+    var firstName = arrayNames[i].slice(0, arrayNames[i].indexOf(' '));
+    var p = document.createElement('p');
+    p.innerHTML = firstName;
+    answerBox.appendChild(p);
+  }
+};
+
+lastNames.onclick = function(){
+for (var i = 0; i < arrayNames.length; i++) {
+    var firstName = arrayNames[i].slice(arrayNames[i].indexOf(' ')+1);
+    var p = document.createElement('p');
+    p.innerHTML = firstName;
+    answerBox.appendChild(p);
+  }
+};
+
+namesWithLength.onclick = function(){
+   for (var i = 0; i < arrayNames.length; i++) {
+    var p = document.createElement('p');
+    p.innerHTML = arrayNames[i] + ' - ' +arrayNames[i].length;
+    answerBox.appendChild(p);
+  }
+};
